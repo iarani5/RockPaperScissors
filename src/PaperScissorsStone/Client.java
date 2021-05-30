cce5ba99e4b6eee600d26818d6bb7018f330c910
iarani5 <iaranizza5@gmail.com>
2021-05-30 15:01:17 -0300

    connection with server response on interface

--- src/PaperScissorsStone/Client.java
+++ src/PaperScissorsStone/Client.java
@@ -18,6 +18,8 @@ import javax.swing.event.ChangeListener;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
@@ -28,8 +30,11 @@ class Client extends JFrame implements ActionListener   {
 
 	// A T R I B U T O S
 	private JButton boton1,boton2,boton3;
-	public JFrame marco;
-	
+	public static JFrame frame;
+	private static JPanel panel;
+	public static JTextField menssage  = new javax.swing.JTextField();
+
+
 	public static final Color MY_Yellow = new Color(255,216,59);
 	
 	private static BufferedReader inFromUser;
@@ -50,25 +55,49 @@ class Client extends JFrame implements ActionListener   {
 
   //  private static String msgRules = "\nRule set:\n - (R)ock beats (S)cissors\n - (S)cissors beats (P)aper\n - (P)aper beats (R)ock\n";
     
+    public void setTitle(String title){
+        frame.setTitle(title); // for this you have declare the frame object as global for this class only
+    }
+    
     //constructor
     public Client() {
     	 //Border emptyBorder = BorderFactory.createEmptyBorder();
-    	 
+    	
+    	 frame = new JFrame();  
+         panel = new JPanel();  
+         panel.setLayout(new FlowLayout());  
+         panel.setBounds(0,0,750,450);
+
 		 setLayout(null);
 	     boton1=new JButton(new ImageIcon("src/img/papel.png"));
 	     boton1.setBounds(230,100,160,220);
-	     add(boton1);
+	     boton1.setBackground(MY_Yellow);
+	     panel.add(boton1);
 	     boton1.addActionListener(this);
 	     
 	     boton2=new JButton(new ImageIcon("src/img/piedra.png"));
 	     boton2.setBounds(10,100,160,220);
-	     add(boton2);
+	     boton2.setBackground(MY_Yellow);
+	     panel.add(boton2);
 	     boton2.addActionListener(this);
 	     
 	     boton3=new JButton(new ImageIcon("src/img/tijera.png"));
 	     boton3.setBounds(450,100,160,220);
-	     add(boton3);
-	     boton3.addActionListener(this);        	
+	     boton3.setBackground(MY_Yellow);
+	     panel.add(boton3);
+	     boton3.addActionListener(this);  
+	     
+	     panel.setBackground(MY_Yellow);
+	     
+	     menssage.setBounds(250,320,300,20);
+	     frame.add(menssage);
+         
+	     frame.add(panel);  
+
+	     frame.setBounds(0,0,850,450);
+	     frame.setVisible(true);
+	     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		    
      }
     
     public static void main(String args[]) throws Exception {
@@ -81,33 +110,31 @@ class Client extends JFrame implements ActionListener   {
     			clientSocket.getOutputStream());
     	Client.inFromServer = new BufferedReader(new InputStreamReader(
     			clientSocket.getInputStream()));
-
-
-
-	JLabel label1=new JLabel("Sistema de Facturación.");
-    label1.setBounds(10,20,300,30);
      
+	 	Client formulario1 = new Client();
+
+    	// TITLE PLAYER 
 	 	try {
 	 		Client.inFromServer = new BufferedReader(new InputStreamReader(
 	 				clientSocket.getInputStream()));
 	 		String response = inFromServer.readLine();
-	 		label1 = new JLabel(response);
+	 		//JLabel title = new JLabel(response);
+	 		
+	 		frame.setTitle(response);
+	 		//title.setFont(new Font("Verdana", Font.PLAIN, 13));
+	 		menssage.setText(response);
+
 	 	    System.out.println(response);
-	 	    System.out.println(label1);
+	 	    //formulario1.getContentPane().add(title);
 	 	 
 	 	} catch (IOException e2) {
 	 		// TODO Auto-generated catch block
 	 		e2.printStackTrace();
 	 	}
-	 
-	 	Client formulario1 = new Client();
-	    formulario1.setBounds(0,0,650,450);
-	    formulario1.setVisible(true);
-	    formulario1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    formulario1.getContentPane().setBackground(MY_Yellow);
-	    formulario1.getContentPane().add(label1);
+	
+	    //formulario1.getContentPane().add(label1);
 
-	 	// add(titulo);
+	    // add(titulo);
 	    
 	//while (true){
         
